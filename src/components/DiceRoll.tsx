a1cf76c0365cf9485198ffd7251784ee1962edf4
Marc Miller <marc.miller@email.saintleo.edu>
2019-10-12 13:58:53 -0400

    Finished drawing layout

--- src/components/DiceRoll.tsx
+++ src/components/DiceRoll.tsx
@@ -5,28 +5,83 @@ import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Table from "react-bootstrap/Table";
 
-const DiceRoll: React.FC = () => {
-  return (
-    <Navbar fixed="bottom">
-      <Container>
-        <Table borderless>
-          <tr>
-            <td>
-              <Button>Roll Dice</Button>
-            </td>
-            <td>&nbsp;</td>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>&nbsp;</td>
-          </tr>
-        </Table>
-      </Container>
-    </Navbar>
-  );
-};
+interface DiceRollState {
+  currentRoll: Array<number>;
+}
+
+class DiceRoll extends React.Component<{}, DiceRollState> {
+  constructor(props: DiceRollState) {
+    super(props);
+    this.state = {
+      currentRoll: []
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.selectDie = this.selectDie.bind(this);
+  }
+
+  handleClick = () => {
+    this.setState({
+      currentRoll: this.rollDice()
+    });
+  };
+
+  selectDie = () => {
+    this.setState({});
+  };
+
+  rollDice() {
+    let newRoll: Array<number> = [];
+    for (let i = 0; i < 5; i++) {
+      newRoll = newRoll.concat([Math.floor(Math.random() * Math.floor(5)) + 1]);
+    }
+    return newRoll;
+  }
+
+  render() {
+    const dice: Array<string> = [
+      "",
+      "fas fa-dice-one fa-5x",
+      "fas fa-dice-two fa-5x",
+      "fas fa-dice-three fa-5x",
+      "fas fa-dice-four fa-5x",
+      "fas fa-dice-five fa-5x",
+      "fas fa-dice-six fa-5x"
+    ];
+
+    return (
+      <Navbar fixed="bottom">
+        <Container>
+          <Table borderless>
+            <tr>
+              <td>
+                <Button onClick={this.handleClick}>Roll Dice</Button>
+                <br />
+                Click dice you want to keep
+              </td>
+              <td>&nbsp;</td>
+              <td>
+                <i className={dice[this.state.currentRoll[0]]}> </i>
+              </td>
+              <td>
+                <i className={dice[this.state.currentRoll[1]]}> </i>
+              </td>
+              <td>
+                <i className={dice[this.state.currentRoll[2]]}> </i>
+              </td>
+              <td>
+                <i className={dice[this.state.currentRoll[3]]}> </i>
+              </td>
+              <td>
+                <i className={dice[this.state.currentRoll[4]]}> </i>
+              </td>
+              <td>&nbsp;</td>
+            </tr>
+          </Table>
+        </Container>
+      </Navbar>
+    );
+  }
+}
 
 export default DiceRoll;
--- src/components/ScoreCard.tsx
+++ src/components/ScoreCard.tsx
@@ -3,46 +3,105 @@ import React from "react";
 import Table from "react-bootstrap/Table";
 import Container from "react-bootstrap/Container";
 
-const ScoreCard: React.FC = () => {
-  const tableHeadings: Array<string> = [
-    "Upper Section",
-    "How to Score",
-    "Game #1",
-    "Game #2",
-    "Game #3",
-    "Game #4",
-    "Game #5",
-    "Game #6"
-  ];
+interface ScoreCardProps {
+  tableHeadings: Array<string>;
+  upperScoreCard: Array<{ label: string; guide: string; game: number }>;
+  lowerScoreCard: Array<{ label: string; guide: string; game: number }>;
+}
 
-  return (
-    <Container>
-      <Table bordered size="sm">
-        <thead>
-          <tr>
-            <th>{tableHeadings[0]}</th>
-            <th>{tableHeadings[1]}</th>
-            <th>{tableHeadings[2]}</th>
-            <th>{tableHeadings[3]}</th>
-            <th>{tableHeadings[4]}</th>
-            <th>{tableHeadings[5]}</th>
-            <th>{tableHeadings[6]}</th>
-            <th>{tableHeadings[7]}</th>
-          </tr>
-        </thead>
+class ScoreCard extends React.Component<{}, ScoreCardProps> {
+  constructor(props: ScoreCardProps) {
+    super(props);
+
+    this.state = {
+      tableHeadings: [
+        "Upper Section",
+        "How to Score",
+        "Game #1",
+        "Game #2",
+        "Game #3",
+        "Game #4",
+        "Game #5",
+        "Game #6"
+      ],
+      upperScoreCard: [
+        { label: "Aces", guide: "Count and add only Aces", game: -1 },
+        { label: "Twos", guide: "Count and add only Twos", game: -1 },
+        { label: "Threes", guide: "Count and add only Threes", game: -1 },
+        { label: "Fours", guide: "Count and add only Fours", game: -1 },
+        { label: "Fives", guide: "Count and add only Fives", game: -1 },
+        { label: "Sixes", guide: "Count and add only Sixes", game: -1 },
+        { label: "Total Score", guide: "==>", game: -1 },
+        { label: "Bonus", guide: "Score 35", game: -1 },
+        { label: "Total", guide: "==>", game: -1 }
+      ],
+      lowerScoreCard: [
+        { label: "3 of a kind", guide: "Score total of all dice", game: -1 },
+        { label: "4 of a kind", guide: "Score total of all dice", game: -1 },
+        { label: "Full House", guide: "Score 25", game: -1 },
+        { label: "Sm Straight", guide: "Score 30", game: -1 },
+        { label: "Lg Straight", guide: "Score 40", game: -1 },
+        { label: "Yahtzee", guide: "Score 50", game: -1 },
+        { label: "Chance", guide: "Score total of all dice", game: -1 },
+        { label: "Bonus Yahtzee", guide: "Score 100 each", game: -1 },
+        { label: "Upper Total", guide: "==>", game: -1 },
+        { label: "Lower Total", guide: "==>", game: -1 },
+        { label: "Grand Total", guide: "==>", game: -1 }
+      ]
+    };
+  }
+
+  drawHeadings() {
+    return this.state.tableHeadings.map(heading => {
+      return (
+        <td>
+          <h6>{heading}</h6>
+        </td>
+      );
+    });
+  }
+
+  drawScoreCard(section: { label: string; guide: string; game: number }[]) {
+    return section.map(row => {
+      const { label, guide, game } = row;
+      return (
         <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
+          <td>
+            <h6>{label}</h6>
+          </td>
+          <td>
+            <small>{guide}</small>
+          </td>
+          <td>{game !== -1 ? game : ""}</td>
+          <td>{game !== -1 ? game : ""}</td>
+          <td>{game !== -1 ? game : ""}</td>
+          <td>{game !== -1 ? game : ""}</td>
+          <td>{game !== -1 ? game : ""}</td>
+          <td>{game !== -1 ? game : ""}</td>
         </tr>
-      </Table>
-    </Container>
-  );
-};
+      );
+    });
+  }
 
+  render() {
+    return (
+      <Container>
+        <Table bordered hover size="sm">
+          <thead>
+            <tr>{this.drawHeadings()}</tr>
+          </thead>
+          <tbody>
+            {this.drawScoreCard(this.state.upperScoreCard)}
+            <tr>
+              <td>
+                <h6>Lower Section</h6>
+              </td>
+            </tr>
+            {this.drawScoreCard(this.state.lowerScoreCard)}
+          </tbody>
+        </Table>
+      </Container>
+    );
+  }
+}
 export default ScoreCard;
